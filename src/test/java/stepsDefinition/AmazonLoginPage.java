151508b5fed8b84cfec7de71863c0af083c28a59
Abhishek Das <abhi.das2007das@gmail.com>
2017-04-26 00:38:53 +0530

    v2 added change 1

--- src/test/java/stepsDefinition/AmazonLoginPage.java
+++ src/test/java/stepsDefinition/AmazonLoginPage.java
@@ -20,7 +20,7 @@ import pageObjects.AmazonHomePage;
  *
  */
 public class AmazonLoginPage {
-	
+	//change 1 - done in localAbhiBranch
 	private MyDriver driver;
 	private AmazonHomePage amazonLoginPageObj;
 	
