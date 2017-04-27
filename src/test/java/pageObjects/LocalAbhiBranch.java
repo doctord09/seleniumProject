1060a961eb4c3304ff6abbf9d49f9026ef555407
Abhishek Das <abhi.das2007das@gmail.com>
2017-04-27 11:54:13 +0530

    change in LocalAbhiBranch - change#2

--- src/test/java/pageObjects/LocalAbhiBranch.java
+++ src/test/java/pageObjects/LocalAbhiBranch.java
@@ -9,6 +9,7 @@ public class LocalAbhiBranch {
 		out.println("I am in Master");
 		System.out.println("want to squash - change for EGIT practice");
 		out.println("commit-1");
+		out.println("commit-2");
 	}
 
 }
