8fb506e5f6e5763c7b110be78f85d42caa657fb9
Abhishek Das <abhi.das2007das@gmail.com>
2017-04-27 11:28:21 +0530

    Checking in Master - including static import

--- src/test/java/pageObjects/LocalAbhiBranch.java
+++ src/test/java/pageObjects/LocalAbhiBranch.java
@@ -1,6 +1,12 @@
 package pageObjects;
-
+import static java.lang.System.out;
 public class LocalAbhiBranch {
 	// new file that got created.
+	/**
+	 * this may cause ambiguity - as class may be in both the static import
+	 */
+	public void myFunc (){
+		out.println("I am in Master");
+	}
 
 }
